da0a192a207c355ce9c8ea6b4b42b79a1f4068b1
ShenTong-2050 <sttya@qq.com>
2021-02-02 15:15:57 +0800

    接收消息（多个消费者）

---
+++ database/rabbitmq/simple/receive2/receive2.go
@@ -0,0 +1,11 @@
+package receive2
+
+import (
+	"database/database/rabbitmq/simple/send"
+)
+
+// Receive 接收消息
+func Receive() {
+	rabbitmq := send.NewRabbitMQSimple("test")
+	rabbitmq.ConsumeSimple()
+}
