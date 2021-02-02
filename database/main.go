503d5c4442cf4a9a9c113248b187bcd4a45be2e1
ShenTong-2050 <sttya@qq.com>
2021-02-02 15:19:20 +0800

    修改测试消息类型

--- database/main.go
+++ database/main.go
@@ -1,22 +1,35 @@
 package main
 
 import (
-	"database/database/rabbitmq"
-	"fmt"
+	rabbitmq "database/database/rabbitmq/publish"
 )
 
 func main() {
 
-	fmt.Println("11")
+	// fmt.Println("11")
 
 	// Rmqsend 测试rabbitmq发送
 	// rabbitmq.Rmqsend()
 
-	//Rmqsend 测试rabbitmq发送
-	rabbitmq.Rmqsend()
+	// simple(一对一)模式推送消息
 
-	//Rmqreceive 测试rabbitmq接收
-	rabbitmq.Rmqreceive()
+	//Rmqsend 测试rabbitmq发送
+	// rabbitmq.Rmqsend()
+
+	// //Rmqreceive 测试rabbitmq接收
+	// rabbitmq.Rmqreceive()
+
+	// //Rmqreceive2 测试多个消费者
+	// rabbitmq.Rmqreceive2()
+
+	/**
+	 * fanout 类型
+	 */
+	rabbitmq.Publish()
+
+	rabbitmq.Customer1()
+
+	rabbitmq.Customer2()
 
 	//mysql.Mysql()
 }
