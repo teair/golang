fa1d7c817469c4defefbd45487c432b7c89243c0
ShenTong-2050 <sttya@qq.com>
2021-03-03 15:16:14 +0800

    表单内容过滤、正则过滤

--- demo/web/web.go
+++ demo/web/web.go
@@ -2,11 +2,12 @@ package web
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
-	"net/url"
+	"regexp"
+	"shentong/demo/beeku"
 	"strings"
+	"time"
 )
 
 // Hello 搭建web服务
@@ -26,26 +27,104 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method) // 获取请求的方法
 	if r.Method == "GET" {
-		t, _ := template.ParseFiles("login.html")
-		fmt.Println(t.Execute(w, nil))
+		cstZone := time.FixedZone("CST", 8*3600) // 东八
+		fmt.Println(time.Now().In(cstZone).Format("2006-01-02 15:04:05"))
+
+		// 判断日期和时间是否合法
+		// t := time.Date(2021, time.Now().Month(), time.Now().Day(), time.Now().Hour(), time.Now().Minute(), time.Now().Second(), 0, time.UTC)
+		// fmt.Printf("Go launched at %s\n", t.Local())
+
+		// t, _ := template.ParseFiles("login.html")
+		// fmt.Println(t.Execute(w, nil))
 		// fmt.Println(t)
 
 	} else {
-		v := url.Values{}
-		v.Set("aa", "11")
-		v.Add("bb", "22")
 
 		// 请求的是登陆数据，那么执行登陆的逻辑判断
 		r.ParseForm()
-		fmt.Println(r.Form)
-		fmt.Println("username:", r.Form["username"])
-		fmt.Println("password:", r.Form["password"])
 
-		// fmt.Println(v)
-		// fmt.Println(v.Get("aa"))
-		// fmt.Println(v.Get("bb"))
+		// if len(r.FormValue("username")) == 0 {
+		// 	fmt.Fprintf(w, "请输入用户名")
+		// 	return
+		// }
+
+		// 正则匹配中文
+		if chinese, _ := regexp.MatchString("^\\p{Han}{2,5}$", r.FormValue("username")); !chinese {
+			fmt.Fprintln(w, "用户名位2-5位汉字!")
+			return
+		}
+
+		// 验证用户输入内容
+		// _, err := strconv.Atoi(r.Form.Get("password"))
+
+		// if err != nil {
+		// 	// 字符串转数字出错，说明不是数字
+		// 	fmt.Fprint(w, "密码必须为数字")
+		// 	return
+		// }
+
+		// if len(r.FormValue("password")) < 3 {
+		// 	fmt.Fprintf(w, "密码长度不得小于3位")
+		// 	return
+		// }
+
+		// if len(r.FormValue("password")) > 6 {
+		// 	// 密码长度不得大于6位
+		// 	fmt.Fprint(w, "密码长度不得大于6位")
+		// 	return
+		// }
+
+		// 正则匹配数字
+		if m, _ := regexp.MatchString("^[0-9]{3,6}$", r.FormValue("password")); !m {
+			fmt.Fprintln(w, "密码为3-6位数字!")
+			return
+		}
+
+		// 判断喜爱的水果是否存在
+		exist := Exists(r.FormValue("favorite"))
+		if exist == false {
+			fmt.Fprintln(w, "干点正事吧铁铁!")
+			return
+		}
+
+		// 判断用户是否选择喜爱的运动
+		fs := Sports(r.Form["interest"])
+
+		if fs == false {
+			fmt.Fprintln(w, "未知的爱好？")
+			return
+		}
+
+		fmt.Fprintln(w, r.FormValue("username"))
+		fmt.Fprintln(w, r.FormValue("password"))
+		fmt.Fprintln(w, r.FormValue("favorite"))
 	}
 
+}
+
+// Sports 用户喜爱的运动
+func Sports(favorite []string) bool {
+	slice := []string{"football", "basketball", "tennis"}
+	a := beeku.SliceDiff(favorite, slice)
+	if a == nil {
+		return true
+	}
+	return false
+}
+
+// Exists 判断喜爱的水果类型是否存在
+func Exists(s string) bool {
+
+	// 判断下拉选项值是否存在
+	slice := []string{"apple", "pear", "banana", "pineapple", "strawberry", "peach", "cherry"}
+
+	for _, fruit := range slice {
+		if fruit == s {
+			return true
+		}
+	}
+
+	return false
 }
 
 // Webmain web服务入口函数
