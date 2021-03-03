a10172003e0c7b713fe6dcb62e363d244c4a13d4
ShenTong-2050 <sttya@qq.com>
2021-03-03 15:17:57 +0800

    入口修改

--- demo/main.go
+++ demo/main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
-	typefunc "shentong/demo/typefunc"
+	web "shentong/demo/web"
 )
 
 func main() {
 
-	slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	// slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 
-	typefunc.TypeFunc(slice)
+	// typefunc.TypeFunc(slice)
+
+	// fmt.Println("=============")
+
+	// 结构体初始化
+	// gostruct.TestStruct()
+
+	// 结构体对象
+	// gostruct.CalcArea()
+
+	// 盒子对象
+	// gostruct.ExtendMethod()
+
+	web.Webmain()
 }
