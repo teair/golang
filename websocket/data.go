af550a22cebaab1249db7057321f68f28aa84d0c
ShenTong-2050 <sttya@qq.com>
2021-03-26 12:45:18 +0800

    修改

--- websocket/data.go
+++ websocket/data.go
@@ -1,10 +1,10 @@
 package main
 
 type Data struct {
-    Ip       string   `json:"ip"`
-    User     string   `json:"user"`
-    From     string   `json:"from"`
-    Type     string   `json:"type"`
-    Content  string   `json:"content"`
-    UserList []string `json:"user_list"`
+	Ip       string   `json:"ip"`
+	User     string   `json:"user"`
+	From     string   `json:"from"`
+	Type     string   `json:"type"`
+	Content  string   `json:"content"`
+	UserList []string `json:"user_list"`
 }
