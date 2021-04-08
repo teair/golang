12540fb3f6968164800a9fa1f2f777d87cb18c6c
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-04-08 15:00:45 +0800

    websocket

---
+++ pangocontext/websocket/server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	go h.run()
+	router.HandleFunc("/stt", myws)
+	if err := http.ListenAndServe("0.0.0.0:7978", router); err != nil {
+		fmt.Println("err:", err)
+	}
+}
