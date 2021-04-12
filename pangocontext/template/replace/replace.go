81bcec8a7bb96427d69adb27087d80e0ac210d0d
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-04-12 10:22:14 +0800

    规范模板替换函数命名与书写格式

--- pangocontext/template/replace/replace.go
+++ pangocontext/template/replace/replace.go
@@ -2,9 +2,9 @@ package replace
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 	"strings"
+	"text/template"
 )
 
 type Friend struct {
@@ -37,50 +37,52 @@ func EmailDealWith(args ...interface{}) string {
 	}
 
 	// replace the @ by " at "
-	return (substrs[0] + "at" + substrs[1])
+	return substrs[0] + "at" + substrs[1]
 }
 
-func ReplaceMain(w http.ResponseWriter, r *http.Request) {
+func ReplacesMain(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		f1 := Friend{Fname: "刘备"}
 
-	f1 := Friend{Fname: "刘备"}
+		f2 := Friend{Fname: "关羽"}
 
-	f2 := Friend{Fname: "关羽"}
+		f3 := Friend{Fname: "张飞"}
 
-	f3 := Friend{Fname: "张飞"}
+		// 控制台输出模板内容
+		// t := template.New("fieldname example")
+		// t = t.Funcs(template.FuncMap{"emailDeal": EmailDealWith})
+		// t.Parse(`
+		// 	hello {{.UserName}}!
+		// 	{{range .Emails}}
+		// 		an emails {{.|emailDeal}}
+		// 	{{end}}
+		// 	{{with .Friends}}
+		// 		{{range .}}my friend name is {{.Fname}}
+		// 		{{end}}
+		// 	{{end}}
+		// `)
 
-	// 控制台输出模板内容
-	// t := template.New("fieldname example")
-	// t = t.Funcs(template.FuncMap{"emailDeal": EmailDealWith})
-	// t.Parse(`
-	// 	hello {{.UserName}}!
-	// 	{{range .Emails}}
-	// 		an emails {{.|emailDeal}}
-	// 	{{end}}
-	// 	{{with .Friends}}
-	// 		{{range .}}my friend name is {{.Fname}}
-	// 		{{end}}
-	// 	{{end}}
-	// `)
+		// p := Person{
+		// 	UserName: "ShenTongtong",
+		// 	Emails:   []string{"1111@qq.com", "2222@qq.com", "3333@qq.com"},
+		// 	Friends:  []*Friend{&f1, &f2, &f3},
+		// }
 
-	// p := Person{
-	// 	UserName: "ShenTongtong",
-	// 	Emails:   []string{"1111@qq.com", "2222@qq.com", "3333@qq.com"},
-	// 	Friends:  []*Friend{&f1, &f2, &f3},
-	// }
+		// t.Execute(os.Stdout, &p)
 
-	// t.Execute(os.Stdout, &p)
-
-	// 浏览器访问模板页
-	t := template.New(`template/html/replace.html`)
-	t = t.Funcs(template.FuncMap{"emailDeal": EmailDealWith})
-	t, _ = t.ParseFiles(`template/html/replace.html`)
-	p := Person{
-		UserName: "ShenTongtong",
-		Emails:   []string{"1111@qq.com", "2222@qq.com", "3333@qq.com"},
-		Friends:  []*Friend{&f1, &f2, &f3},
+		// 浏览器访问模板页
+		t := template.New(`template/html/replace.html`)
+		t = t.Funcs(template.FuncMap{"emailDeal": EmailDealWith})
+		t, _ = t.ParseFiles(`template/html/replace.html`)
+		p := Person{
+			UserName: "ShenTongtong",
+			Emails:   []string{"1111@qq.com", "2222@qq.com", "3333@qq.com"},
+			Friends:  []*Friend{&f1, &f2, &f3},
+		}
+		if err := t.ExecuteTemplate(w, "replace.html", p); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+	} else {
+		fmt.Println("Sorry....")
 	}
-	if err := t.ExecuteTemplate(w, "replace.html", p); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-	}
-	// t.Execute(w, &p)
 }
