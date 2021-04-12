364c499132a44a069c6ccd48b2dce1dee933b9ac
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-04-12 10:02:43 +0800

    修改

--- websocket/server.go
+++ websocket/server.go
@@ -7,11 +7,12 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func main() {
+// SocketMain 测试
+func SocketMain() {
 	router := mux.NewRouter()
 	go h.run()
 	router.HandleFunc("/stt", myws)
-	if err := http.ListenAndServe("0.0.0.0:7978", router); err != nil {
+	if err := http.ListenAndServe("127.0.0.1:7978", router); err != nil {
 		fmt.Println("err:", err)
 	}
 }
