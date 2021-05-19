f97057893c6505844b47a673da1b8d8e7d3081fb
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-05-19 16:07:45 +0800

    后台路由文件

--- bego/routers/admin.go
+++ bego/routers/admin.go
@@ -3,12 +3,19 @@ package routers
 import (
 	"bego/admin"
 	"github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web/context"
 )
 
 // 后台路由配置文件
 func init() {
-	// 自动匹配路由
-	web.AutoRouter(&admin.LoginController{})
-	// 登录页路由
-	//web.Router("/login",&admin.LoginController{})
+	ns := web.NewNamespace("/admin",
+		web.NSCond(func(ctx *context.Context) bool {
+			if ctx.Input.Domain() == "www.wsst.vip" {
+				return true
+			}
+			return false
+		}),
+		web.NSRouter("/test/:string", &admin.TestController{}, "get:Test"),
+	)
+	web.AddNamespace(ns)
 }
