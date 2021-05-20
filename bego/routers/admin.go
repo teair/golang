8455162ed492a752bb0b5998ec3ce0b73f3df569
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-05-20 17:51:52 +0800

    修改后台路由文件

--- bego/routers/admin.go
+++ bego/routers/admin.go
@@ -8,6 +8,14 @@ import (
 
 // 后台路由配置文件
 func init() {
+
+	/*web.Get("/", func(ctx *context.Context) {
+		if ctx.Input.Domain() != "admin.wsst.vip" {
+			ctx.Redirect(401,"/")
+		}
+	})*/
+
+	// 注册后台命名空间
 	ns := web.NewNamespace("/admin",
 		web.NSCond(func(ctx *context.Context) bool {
 			if ctx.Input.Domain() == "www.wsst.vip" {
@@ -15,7 +23,15 @@ func init() {
 			}
 			return false
 		}),
-		web.NSRouter("/test/:string", &admin.TestController{}, "get:Test"),
+		web.NSBefore(func(ctx *context.Context) {
+			web.SetStaticPath("/static", "static")
+			ctx.CheckXSRFCookie()
+		}),
+		// 测试控制器
+		web.NSRouter("/test", &admin.TestController{}, "get:Test"),
+		// 登录页
+		web.NSRouter("/", &admin.LoginController{}, "get:Index"),
+		web.NSRouter("/login", &admin.LoginController{}, "post:Post"),
 	)
 	web.AddNamespace(ns)
 }
