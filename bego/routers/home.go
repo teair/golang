1831eeb0fccae2ca941313e78699752352bf8007
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-05-24 11:49:58 +0800

    测试前台路由配置文件

--- bego/routers/home.go
+++ bego/routers/home.go
@@ -3,22 +3,9 @@ package routers
 import (
 	"bego/home"
 	"github.com/beego/beego/v2/server/web"
-	"github.com/beego/beego/v2/server/web/context"
 )
 
 // 前台路由配置文件
 func init() {
-	ns := web.NewNamespace("/home",
-		web.NSCond(func(ctx *context.Context) bool {
-			if ctx.Input.Domain() == "www.wsst.vip" {
-				return true
-			}
-			return false
-		}),
-		web.NSBefore(func(ctx *context.Context) {
-
-		}),
-		web.NSRouter("/test/:string", &home.TestController{}, "get:Test"),
-	)
-	web.AddNamespace(ns)
+	web.Router("/", &home.TestController{}, "get:Test")
 }
