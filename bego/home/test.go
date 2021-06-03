0c15332e67cb4e8336bf8d14ecf186b257100876
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-06-03 16:18:05 +0800

    修改前台控制器

--- bego/home/test.go
+++ bego/home/test.go
@@ -1,11 +1,20 @@
 package home
 
-import "github.com/beego/beego/v2/server/web"
+import (
+	"fmt"
+	"github.com/beego/beego/v2/server/web"
+)
 
 type TestController struct {
 	web.Controller
 }
 
 func (t *TestController) Test() {
-	t.Ctx.WriteString("home test!")
+	//mark_online := t.Ctx.Input.Param(":mark_online")
+	//app_name := t.Ctx.Input.Param(":app_name")
+	//fmt.Println("mark_online:",mark_online)
+	//fmt.Println("app_name:",app_name)
+	data := t.Ctx.Input.Params()
+	fmt.Println("data=>", data)
+	t.TplName = "home/test/test.html"
 }
