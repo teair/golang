3f119589f49089ca490f4f10bf3b050910223a96
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-06-03 16:18:56 +0800

    修改前台路由

--- bego/routers/home.go
+++ bego/routers/home.go
@@ -8,4 +8,5 @@ import (
 // 前台路由配置文件
 func init() {
 	web.Router("/", &home.TestController{}, "get:Test")
+	web.Router("/test/?:mark_online:int/?:app_name:string", &home.TestController{}, "get:Test") // api/12与api/ 都可匹配
 }
