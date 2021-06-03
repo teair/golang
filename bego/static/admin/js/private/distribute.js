3402473fdbb71e3232c646e6b512d7027c982083
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-06-03 16:20:01 +0800

    添加游戏上/下线功能

--- bego/static/admin/js/private/distribute.js
+++ bego/static/admin/js/private/distribute.js
@@ -14,24 +14,24 @@ $(function(){
 
      $("#all").on('click',function(){
        // alert("查询全部游戏")
-       window.location.href='/admin/index/mark_online/2';
+       window.location.href='/admin/index/2';
      })
 
      $("#typeon").on('click',function(){
         // 上线
-        window.location.href='/admin/index/mark_online/1';
+        window.location.href='/admin/index/1';
      })
 
      $("#typeoff").on('click',function(){
         // 下线
-        window.location.href='/admin/index/mark_online/0';
+        window.location.href='/admin/index/0';
      })
 
      $("#query").on('click',function(){
        // alert("游戏名称查询")
        var app_name = $('#query-name').val();
        // window.location.href='/Index/index/app_name/' + encodeURIComponent(app_name);
-       window.location.href='/admin/index/app_name/' + app_name;
+       window.location.href='/admin/index//' + app_name;
      })
 
      $(".upload").on('click',function(){
