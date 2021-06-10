4cf4a77dba0c1fc8d3697aee16986bfeddc2af43
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-06-10 14:27:14 +0800

    新增公共分页操作

--- bego/admin/base.go
+++ bego/admin/base.go
@@ -1,7 +1,7 @@
 package admin
 
 import (
-	"github.com/beego/beego/v2/core/utils"
+	"github.com/beego/beego/v2/core/utils/pagination"
 	"github.com/beego/beego/v2/server/web"
 )
 
@@ -10,19 +10,19 @@ type baseController struct {
 	web.Controller
 }
 
-func (b *baseController) Prepare() {
+func (this *baseController) Prepare() {
 	// 后台验证登陆
-	if b.GetSession("username") == nil || b.GetSession("password") == nil {
+	if this.GetSession("username") == nil || this.GetSession("password") == nil {
 		// 没有登陆跳转至登录页
 		//b.Ctx.WriteString("please login!")
-		b.Ctx.Redirect(302, "/admin")
+		this.Ctx.Redirect(302, "/admin")
 		return
 	}
 }
 
 // 公共分页操作
-func (this *baseController) SetPaginator(per int, nums int64) *utils.Paginator {
-	p := utils.NewPaginator(this.Ctx.Request, per, nums)
+func (this *baseController) SetPaginator(per int, nums int64) *pagination.Paginator {
+	p := pagination.NewPaginator(this.Ctx.Request, per, nums)
 	this.Data["paginator"] = p
 	return p
 }
