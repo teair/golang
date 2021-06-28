57532ebf0a38ee3a2eb3456665298c8604492951
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-06-28 14:56:29 +0800

    创建分发游戏

--- bego/admin/game.go
+++ bego/admin/game.go
@@ -1,9 +1,48 @@
 package admin
 
+import (
+	"bego/models/admin"
+	"strings"
+)
+
 type GameController struct {
 	baseController
 }
 
 func (this *baseController) GameAdd() {
+	data := this.Ctx.Input.Query("msg")
+	exp := strings.Split(data, "^")
+	if data == "" || len(exp) != 8 {
+		this.Data["json"] = ReturnData{
+			Code: 402,
+			Data: nil,
+			Info: "参数异常...",
+		}
+		this.ServeJSON()
+	}
+	gid, app_name, app_rename, game_size, publicity, fuli, source, game_introduce := exp[0], exp[1], exp[2], exp[3], exp[4], exp[5], exp[6], exp[7]
 
+	gameinfo := admin.GameInfo{
+		Gid:           gid,
+		AppName:       app_name,
+		AppRename:     app_rename,
+		GameSize:      game_size,
+		Source:        source,
+		Publicity:     publicity,
+		Fuli:          fuli,
+		GameIntroduce: game_introduce,
+	}
+
+	// 入库
+	id := admin.GameAdd(gameinfo)
+
+	if id == 0 {
+		this.Data["json"] = ReturnData{
+			Code: 500,
+			Data: nil,
+			Info: "创建失败...",
+		}
+		this.ServeJSON()
+	}
+	this.Redirect("/admin/index", 302)
 }
