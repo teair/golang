d8572974ee03e69eac0e1c3a8dfed93d4c819c56
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-06-28 14:55:10 +0800

    系统常量文件

---
+++ bego/common/const.go
@@ -0,0 +1,19 @@
+package common
+
+// 定义系统常量文件
+
+const GAME_LOGO = "gamelogo"            // 游戏logo
+const GAME_THUMB = "gamethumb"          // 游戏图标
+const GMAE_LUNBO = "gamelunbo"          // 首页轮播
+const GAME_HOTREC = "gamehotrec"        // 热门推荐
+const GAME_NORMREC = "gamenormrec"      // 正常推荐
+const GAME_OFFICALRBG = "gameofficalbg" // 官网背景
+const GAME_CUT1 = "gamecut1"            // 游戏logo
+const GAME_CUT2 = "gamecut2"            // 游戏截图1
+const GAME_CUT3 = "gamecut3"            // 游戏截图2
+const GAME_CUT4 = "gamecut4"            // 游戏截图3
+const GAME_CUT5 = "gamecut5"            // 游戏截图4
+const SERVER_LIST = "serverlist"        // 游戏截图5
+const WEB_LOGO = "weblogo"              // 网页logo
+const GAME_TYPE = "gametype"            // 游戏类型
+const GAME_THEME = "gametheme"          // 游戏主题
