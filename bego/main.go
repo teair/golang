0cbb68ab690a324c84a2b721db0ba52b56f4ece0
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-08-02 15:19:47 +0800

    新增gorm连接并定义全局对象

--- bego/main.go
+++ bego/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"bego/models"
 	_ "bego/routers"
 	beego "github.com/beego/beego/v2/server/web"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
+
+func init() {
+	var err error
+	dsn, _ := beego.AppConfig.String("gorm::data_source")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	models.DB = db
+}
 
 func main() {
 	beego.Run()
