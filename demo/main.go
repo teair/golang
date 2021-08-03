b28cdc9b89b3c4e953f369639f0b853aeafa8fc3
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-08-03 14:33:22 +0800

    修改测试

--- demo/main.go
+++ demo/main.go
@@ -1,37 +1,14 @@
 package main
 
 import (
+	test2 "demo/test"
 	"fmt"
 	"reflect"
 	"sort"
 )
 
 func main() {
-
-	// slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-
-	// typefunc.TypeFunc(slice)
-
-	// fmt.Println("=============")
-
-	// 结构体初始化
-	// gostruct.TestStruct()
-
-	// 结构体对象
-	// gostruct.CalcArea()
-
-	// 盒子对象
-	// gostruct.ExtendMethod()
-
-	//a := []int{1,2,3,6}
-	//b := []int{1,3,4}
-
-	//dfa,dfb := test(a,b)
-	//fmt.Println(dfa,dfb)
-	//web.Webmain()
-
-	c := app()
-	fmt.Println(reflect.TypeOf(c))
+	test2.TestMain()
 }
 
 func app() func(string) string {
