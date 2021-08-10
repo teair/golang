104243599d1cd8d9a8e7a9757fd46c93c57d7281
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-08-10 10:35:53 +0800

    gorm添加表前缀、使用单数表名

--- bego/main.go
+++ bego/main.go
@@ -6,16 +6,22 @@ import (
 	beego "github.com/beego/beego/v2/server/web"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
 func init() {
 	var err error
 	dsn, _ := beego.AppConfig.String("gorm::data_source")
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   "fy_", // 表名前缀，`User`表为`fy_users`
+			SingularTable: true,  // 使用单数表名，启用该选项后，`User` 表将是`user`
+		},
+	})
 	if err != nil {
 		panic(err)
 	}
-	models.DB = db
+	models.Db = db
 }
 
 func main() {
