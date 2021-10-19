771923c5a2eb58918b5e4c9a68d3f2a0f990d0c0
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-10-19 15:42:09 +0800

    rune类型、字符串默认为utf-8、rune与byte类型的转换、单个中文字符的获取

---
+++ demo/immok/go-3-6.go
@@ -0,0 +1,37 @@
+package immok
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func runes() {
+	s := "Yes我爱中国!" // UTF-8
+
+	for i, ch := range s { // ch is a rune
+		fmt.Printf("(%d %X) ", i, ch)
+	}
+
+	fmt.Println()
+
+	fmt.Println("Rune count:", utf8.RuneCountInString(s))
+
+	bytes := []byte(s)
+
+	for len(bytes) > 0 {
+
+		ch, size := utf8.DecodeRune(bytes)
+
+		// 获取当前字符之后的元素并返回给bytes直到bytes为空
+		bytes = bytes[size:]
+
+		fmt.Printf("%c ", ch)
+	}
+
+	fmt.Println()
+
+	for i, ch := range []rune(s) { // rune => int(32)
+		fmt.Printf("(%d %c) ", i, ch)
+	}
+
+}
