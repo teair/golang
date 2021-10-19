ce7cb1b1f2282ae06bda85682cd41b47c408dc31
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-10-19 15:20:18 +0800

    map 类型的声明、遍历、删除及通过map寻找最长不重复子串

---
+++ demo/immok/go-3-4.go
@@ -0,0 +1,73 @@
+package immok
+
+import "fmt"
+
+func mapMain() {
+
+	m := map[string]int{
+		"PHP":    1,
+		"Golang": 2,
+		"Java":   3,
+		"React":  4,
+		"NodeJs": 5,
+	}
+
+	// map 的声明
+	m1 := make(map[string]int) // empty map
+	m2 := map[string]int{}     // nil
+	fmt.Println(m1, m2)
+
+	fmt.Println("Traversing map")
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+
+	java, ok := m["Java"]
+	fmt.Println(ok)
+	if ok {
+		fmt.Println(java)
+	}
+	fmt.Println("Deleting values")
+	delete(m, "Java")
+	fmt.Println(m["Java"])
+}
+
+// 寻找最长不重复子串
+func lengthOfNonRepeatingSubStr(s string) int {
+
+	start := 0 // 最长不重复子串的起始位置
+
+	maxLength := 0 // 最长不重复子串的长度
+
+	var lastOccured = make(map[byte]int) // 最后一次遇到起始字符的位置
+
+	for i, chr := range []byte(s) {
+
+		// 如果最后遇到的字符索引大于最长不重复子串的起始位置
+		if lastI, ok := lastOccured[chr]; ok && lastI >= start {
+			start = lastOccured[chr] + 1
+		}
+
+		// 如果该不重复子串的长度大于最大不重复子串则更新
+		if i-start+1 > maxLength {
+			maxLength = i - start + 1
+		}
+
+		lastOccured[chr] = i
+	}
+
+	return maxLength
+}
+
+func testLength() {
+	fmt.Println(
+		lengthOfNonRepeatingSubStr("abcabcdabc"))
+	fmt.Println(
+		lengthOfNonRepeatingSubStr("bbbb"))
+	fmt.Println(
+		lengthOfNonRepeatingSubStr("pwwkew"))
+	fmt.Println(
+		lengthOfNonRepeatingSubStr("c"))
+	fmt.Println(
+		lengthOfNonRepeatingSubStr("abcdefg"))
+}
