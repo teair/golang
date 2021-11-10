899530e82f4bb7d2bf22f7a4c6a82882c87514ba
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-11-10 16:49:49 +0800

    常用系统接口

--- demo/immok/interfaces/retriever/mock/retriever.go
+++ demo/immok/interfaces/retriever/mock/retriever.go
@@ -1,7 +1,14 @@
 package mock
 
+import "fmt"
+
 type Retriever struct {
 	Contents string
+}
+
+// String 常用系统接口
+func (r *Retriever) String() string {
+	return fmt.Sprintf("Retriever: {Content=%s}",r.Contents)
 }
 
 func (r *Retriever) Post(url string, form map[string]string) string {
