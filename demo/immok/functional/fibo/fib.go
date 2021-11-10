8cd76b7e54754ef6e5d442e7fab69dd2c77ee750
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-11-10 16:56:10 +0800

    斐波那契数列(给函数实现接口)

---
+++ demo/immok/functional/fibo/fib.go
@@ -0,0 +1,44 @@
+package fibo
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
+// 计算前两个数之和
+func fibonacci() intGen {
+	a, b := 0, 1
+	return func() int {
+		a, b = b, a+b
+		return a
+	}
+}
+
+// 可一给函数实现接口,也就是说函数是一等公民
+type intGen func() int
+
+func (g intGen) Read(p []byte) (n int, err error) {
+	next := g()
+	if next > 1000 {
+		return 0, io.EOF
+	}
+	s := fmt.Sprintf("%d\n", next)
+	return strings.NewReader(s).Read(p)
+}
+
+func printFileContents(reader io.Reader) {
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+//FibMain 斐波那契数列
+func FibMain() {
+
+	f := fibonacci()
+
+	printFileContents(f)
+}
