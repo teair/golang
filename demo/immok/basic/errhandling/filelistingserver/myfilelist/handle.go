b285e35f5671cb0074d039b03c9ddaa6400f2baa
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-11-11 17:20:08 +0800

    默写错误包装代码

---
+++ demo/immok/basic/errhandling/filelistingserver/myfilelist/handle.go
@@ -0,0 +1,40 @@
+package myfilelist
+
+import (
+	"demo/immok/basic/errhandling/filelistingserver/myfilelist/filelist"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+	"os"
+)
+
+type appHandle func(writer http.ResponseWriter, request *http.Request) error
+
+func wrapperHandle(handle appHandle) func(http.ResponseWriter, *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		err := handle(writer, request)
+		if err != nil {
+			log.Warn("Error handle request:", err.Error())
+			code := http.StatusOK
+			switch {
+			case os.IsNotExist(err):
+				code = http.StatusNotFound
+			case os.IsPermission(err):
+				code = http.StatusForbidden
+			default:
+				code = http.StatusInternalServerError
+			}
+			http.Error(
+				writer,
+				http.StatusText(code),
+				code,
+			)
+		}
+	}
+}
+
+func Main() {
+
+	http.HandleFunc("/myfile/", wrapperHandle(filelist.MyFileHandle))
+
+	http.ListenAndServe(":9999", nil)
+}
