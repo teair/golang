25ad2915a8b2074efcf76d4fb94680b34ed1a538
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-11-11 15:12:15 +0800

    树的遍历打印接口

--- demo/immok/basic/tree/traversal.go
+++ demo/immok/basic/tree/traversal.go
@@ -5,7 +5,7 @@ import "fmt"
 // Traverse 遍历结构体(中序遍历)
 func (node *Node) Traverse() {
 	node.TraverseFunc(func(n *Node) {
-		n.Print()
+		n.Print() // 打印这个操作变为动态的【此处是打印每一个树节点】
 	})
 	fmt.Println()
 }
