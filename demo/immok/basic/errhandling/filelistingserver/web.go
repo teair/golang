ac5e80573f8ee3d25185e5ec9466a6753c172b0a
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-11-12 17:27:37 +0800

    增加对外可访问错误

--- demo/immok/basic/errhandling/filelistingserver/web.go
+++ demo/immok/basic/errhandling/filelistingserver/web.go
@@ -9,6 +9,11 @@ import (
 
 type appHandler func(writer http.ResponseWriter, request *http.Request) error
 
+type userError interface {
+	error
+	Message() string
+}
+
 func errorWrapper(handle appHandler) func(http.ResponseWriter, *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
 		err := handle(writer, request)
@@ -16,6 +21,16 @@ func errorWrapper(handle appHandler) func(http.ResponseWriter, *http.Request) {
 
 			// 控制台打印错误
 			log.Warn("Error handling request:", err.Error())
+
+			// 对外可访问错误
+			if userErr, ok := err.(userError); ok {
+				http.Error(
+					writer,
+					userErr.Message(),
+					http.StatusBadRequest,
+				)
+				return
+			}
 
 			code := http.StatusOK
 
@@ -38,7 +53,7 @@ func errorWrapper(handle appHandler) func(http.ResponseWriter, *http.Request) {
 }
 
 func Main() {
-	http.HandleFunc("/list/", errorWrapper(filelisting.HandleFileList))
+	http.HandleFunc("/", errorWrapper(filelisting.HandleFileList))
 	err := http.ListenAndServe(
 		":8888",
 		nil,
