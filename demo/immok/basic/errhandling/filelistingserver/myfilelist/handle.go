01a7b80c13b403acecbb87c7577b938ec1214e96
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-11-12 17:03:36 +0800

    细分返回错误【系统错误、用户可见错误】

--- demo/immok/basic/errhandling/filelistingserver/myfilelist/handle.go
+++ demo/immok/basic/errhandling/filelistingserver/myfilelist/handle.go
@@ -9,13 +9,40 @@ import (
 
 type appHandle func(writer http.ResponseWriter, request *http.Request) error
 
+type userError interface {
+	error
+	Message() string
+}
+
 func wrapperHandle(handle appHandle) func(http.ResponseWriter, *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		// 对于访问未知目录接管异常处理
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("Panic:%v", r)
+				http.Error(
+					writer,
+					http.StatusText(http.StatusInternalServerError),
+					http.StatusInternalServerError,
+				)
+			}
+		}()
 		err := handle(writer, request)
 		if err != nil {
 			//log.Warn("Error handle request:", err.Error())
 			// 默认的log
 			log.Printf("Error occured "+"handle request:%s", err.Error())
+
+			// 判断当前错误是否为对用户可见的错误【用户可见错误】
+			if userErr, ok := err.(userError); ok {
+				http.Error(
+					writer,
+					userErr.Message(), // 给用户看
+					http.StatusBadRequest,
+				)
+				return
+			}
+
 			code := http.StatusOK
 			switch {
 			case os.IsNotExist(err):
@@ -36,7 +63,7 @@ func wrapperHandle(handle appHandle) func(http.ResponseWriter, *http.Request) {
 
 func Main() {
 
-	http.HandleFunc("/myfile/", wrapperHandle(filelist.MyFileHandle))
+	http.HandleFunc("/", wrapperHandle(filelist.MyFileHandle))
 
 	http.ListenAndServe(":9999", nil)
 }
