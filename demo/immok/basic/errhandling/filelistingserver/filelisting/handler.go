866f82958ac0aea54fd624509ab177222752ea33
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-11-12 17:26:58 +0800

    检测请求前缀、增加自定义错误类型

--- demo/immok/basic/errhandling/filelistingserver/filelisting/handler.go
+++ demo/immok/basic/errhandling/filelistingserver/filelisting/handler.go
@@ -4,11 +4,31 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
+const prefix = "/list/"
+
+type userError string
+
+func (e userError) Error() string {
+	return e.Message()
+}
+
+func (e userError) Message() string {
+	return string(e)
+}
+
 func HandleFileList(writer http.ResponseWriter, request *http.Request) error {
+
+	url := request.URL.Path
+	// 非前缀请求
+	if strings.Index(url, prefix) != 0 {
+		return userError("request must be prefix:" + prefix)
+	}
+
 	// 获取文件路径
-	path := request.URL.Path[len("/list/"):] // 匹配/list/后边的参数,也就是指定文件的路径
+	path := request.URL.Path[len(prefix):] // 匹配/list/后边的参数,也就是指定文件的路径
 
 	// 根据文件路径打开文件
 	file, err := os.Open(path) // 打开存在的文件
