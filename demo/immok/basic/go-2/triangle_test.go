ca2ff82bcd364a4145add6a12a4ab33853e675a4
ShenTong-2050 <46037252+ShenTong-2050@users.noreply.github.com>
2021-11-16 09:52:00 +0800

    go搭建测试用例

---
+++ demo/immok/basic/go-2/triangle_test.go
@@ -0,0 +1,21 @@
+package go_2
+
+import (
+	"testing"
+)
+
+// TestTriangle 表格驱动测试
+func TestTriangle(t *testing.T) {
+	tests := []struct{ a, b, c int }{
+		{3, 4, 5},
+		{5, 12, 13},
+		{8, 15, 17},
+		{12, 35, 37},
+		{30000, 40000, 50000},
+	}
+	for _, tt := range tests {
+		if actual := CalcTriangle(tt.a, tt.b); actual != tt.c {
+			t.Errorf("calcTriangle(%d,%d); got %d; expected:%d", tt.a, tt.b, actual, tt.c)
+		}
+	}
+}
