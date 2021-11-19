726f3c1e305eb37944b9dea8a0404a0bf5c7a7d2
ShenTong-2050 <17601077189@163.com>
2021-11-19 14:18:51 +0800

    增加接口注释

--- demo/immok/basic/queue/queue.go
+++ demo/immok/basic/queue/queue.go
@@ -1,19 +1,16 @@
 package queue
 
-// Queue 通过别名来扩展业务类型
+// Queue An FIFO queue
 type Queue []interface{}
 
-// Push func (q *Queue) Push(i interface{}) {
-// 如果想限定加入类型
-//func (q *Queue) Push(i int) {
-// 这种写法编译可以运行时报错
+// Push the element into the queue.
+// 	e.g. q.Push(123)
 func (q *Queue) Push(i interface{}) {
 	*q = append(*q, i)
 	//*q = append(*q, i.(int))
 }
 
-// Pop func (q *Queue) Pop() interface{} {
-//func (q *Queue) Pop() int {
+// Pop element from head.
 func (q *Queue) Pop() interface{} {
 	head := (*q)[0]
 	*q = (*q)[1:]
@@ -24,6 +21,7 @@ func (q *Queue) Pop() interface{} {
 	//return head.(int)
 }
 
+// IsEmpty Returns if the Queue is Empty or not.
 func (q *Queue) IsEmpty() bool {
 	return len(*q) == 0
 }
